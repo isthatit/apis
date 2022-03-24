d4eb6b314a6f4b0f9ce77035b692a5bbd43fc785
isthatit <isthatitdev@gmail.com>
2022-03-24 16:15:05 +0900

    게시판 기능 수정

--- src/apis/board.js
+++ src/apis/board.js
@@ -1,73 +1,103 @@
 
 const router = require("express").Router();
 
-let result = [
+let boardList = [
     {id: 1, title: '1 책 읽으실 분?', content: '1 책 읽으실 분 내용?', count: 10},
     {id: 2, title: '2 책 읽으실 분?', content: '2 책 읽으실 분 내용?', count: 20},
     {id: 3, title: '3 책 읽으실 분?', content: '3 책 읽으실 분 내용?', count: 30},
     {id: 4, title: '4 책 읽으실 분?', content: '4 책 읽으실 분 내용?', count: 40},
     {id: 5, title: '5 책 읽으실 분?', content: '5 책 읽으실 분 내용?', count: 50}
 ];
+let _ids = 5;
+const getIds = () => _ids+=1;
+
+let returnValue = {
+    result: '',
+    detail: '',
+    clean : () => {returnValue.result = ''; returnValue.detail = '';}
+};
 
 router.get('/', (req, res) => {
-    res.json(result);
+    returnValue.clean();
+    returnValue.result = "success";
+    returnValue.detail = boardList;
+    res.json(returnValue);
 });
 
-
+/**
+ * @returns {json} board 게시물 정보
+ */
 router.get('/:boardID', (req, res) => {
-    const boardID = req.params.boardID;
-    const board = result.filter(board=> board.id == boardID);
-    res.json({board});
+    const boardId = req.params.boardID;
+    const board = boardList.filter(board=> board.id == boardId).pop();
+    returnValue.clean();
+    returnValue.result = "success";
+    returnValue.detail = board;
+    res.json(returnValue);
 });
 
 /**
  * 게시물을 등록한다.
  * @param {json} title 제목
  * @param {json} content 내용
+ * @returns {json} returnValue 작업결과
  */
 router.post('/', (req, res) => {
-    console.log(req.body)
+    returnValue.clean();
+    
+    const {title, content} = req.body;
     const addValue = {
-        id : result.length + 1,
-        title : req.body.title,
-        content : req.body.content,
-        count : (result.length + 1) * 10,
+        id : getIds(),
+        title : title,
+        content : content,
+        count : (boardList.length + 1) * 10,
     }
-    console.log(addValue);
-
-    result.push(addValue);
-
-    res.json(result);
+    
+    boardList.push(addValue);
+    returnValue.result =  "success";
+    returnValue.detail = addValue;
+    res.json(returnValue);
 });
 
+/**
+ * 게시물을 수정한다.
+ * @param {json} id 아이디
+ * @param {json} title 제목
+ * @param {json} content 내용
+ * @returns {json} returnValue 작업결과
+ */
 router.put('/', (req, res) => {
     const boardId = req.body.id;
-    const title = req.body.title;
-    const content = req.body.content;
+    const {title, content} = req.body;
 
-    result.filter(board => board.id == boardId).map(board => {
+    boardList.filter(board => board.id == boardId).map(board => {
         
         board.title = title;
         board.content = content
+        returnValue.clean();
+        returnValue.result = "success";
+        returnValue.detail = board;
     });
-
-    console.log(result);
-
-    res.json(result);
+    res.json(returnValue);
 });
 
 
 router.delete('/:boardId', (req, res) => {
     const boardID = req.params.boardId;
-    const board = result.filter(board => board.id == boardID).pop();
+    const board = boardList.filter(board => board.id == boardID).pop();
 
-    const idx = result.findIndex(target => target == board);
+    const idx = boardList.findIndex(target => target == board);
+    console.log(idx);
 
+    returnValue.clean();
     if(idx > -1){
-        result.splice(idx, 1);
+        boardList.splice(idx, 1);
+        returnValue.result = "success";
+    }else{
+        returnValue.result = "fail";
+        returnValue.detail = "does not match"
     }
-
-    res.json(result)
+    res.json(returnValue);
 });
 
 module.exports = router;
